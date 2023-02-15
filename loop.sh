298db903ab44c3ef6b53552e8f1132edde7e491a
Bartek Dobrowolski-Nowakowski <bartek@bnowakowski.pl>
2023-02-15 09:17:13 +0100

    check root privilidges

--- loop.sh
+++ loop.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+# https://stackoverflow.com/a/18216122
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 while true; do ./truetool.sh -s; date; sleep 3600; done
 
